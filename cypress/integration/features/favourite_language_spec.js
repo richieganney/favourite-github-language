56c3a95ec479877ded0645c8849cfc49b1021a8c
richieganney <richieganney@gmail.com>
2019-09-25 17:00:51 +0100

    displays favourite language to user

---
+++ cypress/integration/features/favourite_language_spec.js
@@ -0,0 +1,10 @@
+describe("favourite language", () => {
+
+    it("shows 'users favourite language is:' when user presses enter", () => {
+        cy.visit("localhost:3000");
+        cy.get("#username_search_bar").type(
+        "richieganney"
+        ).type('{enter}')
+        cy.get("#favouriteLanguage").should("contain", "Users favourite language is:")
+    });
+});
--- src/components/FavouriteLanguage.js
+++ src/components/FavouriteLanguage.js
@@ -3,13 +3,24 @@ import PropTypes from "prop-types";
 
 class FavouriteLanguage extends Component {
 
+    mode(languages){
+        return languages.sort((a,b) =>
+        languages.filter(v => v===a).length
+        - languages.filter(v => v===b).length
+        ).pop();
+    }
+
     render() {
+        if(this.props.languages.length !== 0){
         return (
-            <div>
-                 
+            <div id="favouriteLanguage">
+                 <h1>Users favourite language is: {this.mode(this.props.languages)}</h1>
             </div>
         );
+    } else {
+        return null
     }
+}
 }
 
 FavouriteLanguage.propTypes = {
