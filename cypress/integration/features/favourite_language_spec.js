a9337f5fe920f455ca73b5dacde6c601f055e206
richieganney <richieganney@gmail.com>
2019-09-27 00:45:38 +0100

    searchbar clears after user presses enter

--- cypress/integration/features/favourite_language_spec.js
+++ cypress/integration/features/favourite_language_spec.js
@@ -1,10 +1,10 @@
 describe("favourite language", () => {
-    it("shows 'users favourite language is:' when user presses enter", () => {
+    it("shows user a message when they presses enter", () => {
         cy.visit("localhost:3000");
         cy.get("#username_search_bar").type(
         "richieganney"
         ).type('{enter}')
-        cy.get("#favouriteLanguage").should("contain", "favourite language is:")
+        cy.get("#favouriteLanguage").should("contain", "If I could hazard a guess, their favourite language would be")
     });
 
     it("shows nothing when the username is invalid", () => {
